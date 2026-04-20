e8f92b0adfd345c6d6921ca326ea70c086f36ca6
Pixayo <kaio.rodrigo729@gmail.com>
2026-04-20 12:22:44 -0300

    Add: app notification exception

---
+++ src/main/java/github/com/pixayo/timer/AppNotificationException.java
@@ -0,0 +1,7 @@
+package github.com.pixayo.timer;
+
+public class AppNotificationException extends RuntimeException {
+    public AppNotificationException(String message) {
+        super(message);
+    }
+}
